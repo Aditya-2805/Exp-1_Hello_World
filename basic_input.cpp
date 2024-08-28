e2b7f55e7aa62f98e528566445289ee53adf6b2f
Aditya-2805 <adityasthawarmath2008@gmail.com>
2024-08-28 10:56:19 +0530

    Add files via upload

---
+++ basic_input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std; 
+
+int main(){
+    string a;
+    cout<<"Enter an input: "<<endl;
+    cin>>a;
+    cout<<"Output is: "<<a;
+}
+/*
+Enter an input: 
+Aditya
+Output is: Aditya
+*/
